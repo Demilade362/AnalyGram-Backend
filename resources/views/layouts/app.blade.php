70b395676f87f4deccad47d1d1ee45eb8ae4bdef
Oluwademilade Abatan <68753229+Demilade362@users.noreply.github.com>
2024-02-07 16:14:18 -0800

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', ''Analygram For Developers'') }}</title>
+    <title>{{ config('app.name', 'Analygram For Developers') }}</title>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.bunny.net">
