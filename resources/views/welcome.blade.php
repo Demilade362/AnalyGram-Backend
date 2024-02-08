896bc50a59bac0a51a7f72412308f6241465a1a7
Oluwademilade Abatan <68753229+Demilade362@users.noreply.github.com>
2024-02-07 16:08:33 -0800

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -34,7 +34,7 @@
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
-            <a class="navbar-brand" href="#">{{ config('app.name', 'Analygram') }}</a>
+            <a class="navbar-brand" href="#">{{ config('app.name', 'Analygram For Developers') }}</a>
             <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
                 <ul class="navbar-nav nav-underline">
                     <li class="nav-item">
