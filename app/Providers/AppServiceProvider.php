40f0869b5e6cc384e5f72ba315aa0356eaa66afc
ademolademilade362 <ademolademilade362@gmail.com>
2024-02-09 17:42:58 +0000

    Pagination Error Fixed

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,6 @@ class AppServiceProvider extends ServiceProvider
     {
         //
         Schema::defaultStringLength(191);
-        Paginator::useBootstrapFive()
+        Paginator::useBootstrapFive();
     }
 }
