ff08ecfea0f10e0fdc79c128bd78554402f0bd48
ademolademilade362 <ademolademilade362@gmail.com>
2024-02-09 17:06:57 +0000

    features Added

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -45,7 +45,26 @@
                     </li>
                 </ul>
                 <div class="ms-auto">
-                    <a href="/login" class="btn btn-light" type="submit">Admin Login</a>
+                    @auth
+                        <li class="nav-item dropdown">
+                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                {{ Auth::user()->name }}
+                            </a>
+
+                            <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                                <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                    {{ __('Logout') }}
+                                </a>
+
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                    @csrf
+                                </form>
+                            </div>
+                        </li>
+                        @else
+                        <a href="/login" class="btn btn-light" type="submit">Admin Login</a>
+                    @endauth
                 </div>
 
             </div>
