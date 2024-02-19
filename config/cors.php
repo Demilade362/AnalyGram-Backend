86724192324a8b2a59bd40e861b56ba1ca4d18e4
Demilade362 <68753229+Demilade362@users.noreply.github.com>
2024-02-19 22:07:51 +0000

    cors fixed

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['https://analygram-6d0a5.web.app/'],
+    'allowed_origins' => ['*'],
 
     'allowed_origins_patterns' => [],
 
